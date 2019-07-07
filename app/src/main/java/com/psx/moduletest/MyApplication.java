7f87d481f62ab003ee771a4279e765079fa443de
psx95 <pranav.ps95@hotmail.com>
2019-07-07 13:41:43 +0530

    style(MyApplication): Swicthed to lambdas instead of anonymous class

--- app/src/main/java/com/psx/moduletest/MyApplication.java
+++ app/src/main/java/com/psx/moduletest/MyApplication.java
@@ -4,18 +4,12 @@ import android.app.Application;
 import android.widget.Toast;
 
 import com.psx.simplemaths.SimpleMath;
-import com.psx.simplemaths.SimpleMathCallbacks;
 
 public class MyApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        SimpleMath.init(this, new SimpleMathCallbacks() {
-            @Override
-            public void onCalculationCompleted(String performedOperation) {
-                Toast.makeText(getApplicationContext(), "Calculation Done", Toast.LENGTH_SHORT).show();
-            }
-        });
+        SimpleMath.init(this, performedOperation -> Toast.makeText(getApplicationContext(), "Calculation Done", Toast.LENGTH_SHORT).show());
     }
 }
