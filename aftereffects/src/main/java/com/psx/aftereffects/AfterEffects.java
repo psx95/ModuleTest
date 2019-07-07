94707d6c1c7e17eb7d536218f0b1a5f4478c0009
psx95 <pranav.ps95@hotmail.com>
2019-07-08 00:48:03 +0530

    feat(AfterEffects): Adds different after effects for each math operation

--- aftereffects/src/main/java/com/psx/aftereffects/AfterEffects.java
+++ aftereffects/src/main/java/com/psx/aftereffects/AfterEffects.java
@@ -3,7 +3,6 @@ package com.psx.aftereffects;
 import android.animation.Animator;
 import android.util.Log;
 import android.view.View;
-import android.view.animation.Animation;
 import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
@@ -30,38 +29,46 @@ public class AfterEffects {
         }
         switch (operationPerformed) {
             case "ADDITION":
-                final FrameLayout rootView = activityAwareness.getCurrentActivity().findViewById(android.R.id.content);
-                View v = View.inflate(activityAwareness.getCurrentActivity(), R.layout.sucess_view, rootView);
-                v.bringToFront();
-                LottieAnimationView lottieAnimationView = v.findViewById(R.id.animation_view);
-                lottieAnimationView.addAnimatorListener(new Animator.AnimatorListener() {
-                    @Override
-                    public void onAnimationStart(Animator animation) {
-
-                    }
-
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        rootView.removeView(rootView.findViewById(R.id.success_view));
-                    }
-
-                    @Override
-                    public void onAnimationCancel(Animator animation) {
-
-                    }
-
-                    @Override
-                    public void onAnimationRepeat(Animator animation) {
-
-                    }
-                });
+                startSuccessAnimation(R.layout.sucess_view_add);
                 break;
             case "SUBTRACTION":
+                startSuccessAnimation(R.layout.sucess_view_sub);
                 break;
             case "MULTIPLICATION":
+                startSuccessAnimation(R.layout.sucess_view_mult);
                 break;
             case "DIVISION":
+                startSuccessAnimation(R.layout.sucess_view_div);
                 break;
         }
+    }
+
+    private static void startSuccessAnimation(int layoutID) {
+        final FrameLayout rootView = activityAwareness.getCurrentActivity().findViewById(android.R.id.content);
+        View v = View.inflate(activityAwareness.getCurrentActivity(), layoutID, rootView);
+        v.bringToFront();
+        final LottieAnimationView lottieAnimationView = v.findViewById(R.id.animation_view);
+        lottieAnimationView.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                lottieAnimationView.removeAllAnimatorListeners();
+                rootView.removeView(rootView.findViewById(R.id.success_view));
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
     }
 }
